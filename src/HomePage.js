18368955bb81edde5b55abbe0439ab6e8713925a
Tyler Floyd <inglorious_rex@live.com>
2018-11-29 21:19:27 -0800

    1st In Class Lab 5 Work

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,29 +1,89 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
+import {loadAllMessages} from './redux/actions';
+import axios from 'axios';
 
+class Message extends Component {
+    render(){
+        return(
+            <div className="message">
+                {this.props.content}
+            </div>
+        );
+    }
+}
 class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.sendSomeData = this.sendSomeData.bind(this);
+        this.updateMessage = this.updateMessage.bind(this);
+        this.state = {
+            content: null,
+            messageValue: '',
+        };
+    }
+    
+    ComponentDidMount(){
+        axios.get('/api/messages')
+        .then((res) => {
+            console.log(res.data);
+            this.props.loadAllMessages(res.data);
+        }).catch((e) => {
+            console.log(e);
+        });
+    }
+
+    updateMessage(e){
+        this.setState({
+            messageValue: e.target.value,
+        });
+
+    }
+
+    sendSomeData() {
+        axios(
+            {
+                method: 'POST',
+                url: '/api/sendmessage',
+                data:{
+                    message: this.state.messageValue(),
+                }
+            })
+            .then((res)=>{
+             console.log(res)
+            }).catch((e) => {
+                console.log(e);  
+        }); 
+        this.setState({
+            messageValue: '',
+        })        
+        
+    }
+
     render() {
         return (
             <div>
-                <h1>Home Page</h1>
-                <p>
-                    Here is my main page content <Link to="/page1/mail">Mail</Link>
-                </p>
-                <p>
-                    <a href="https://reacttraining.com/react-router/web/guides/quick-start">Click me to find out more about routing</a>
-                </p>
+                <div className="messages">
+                    {
+                        this.props.messages.map((messageData, i) => <Message key={i} content={messageData}/>)
+                    }
+                </div>
+                <input value={this.state.messageValue} onChange={this.updateMessage}/>
+                <button onClick={this.sendSomeData}>Send Message</button>
             </div>
+            
         );
     }
 }
 
 const mapStateToProps = (state, ownProps) => {
     return {
+        messages : state.testReducer.messages,
     };
 };
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {loadAllMessages};
 
 export default connect(
     mapStateToProps,
--- src/redux/testReducer.js
+++ src/redux/testReducer.js
@@ -1,13 +1,20 @@
 const initialState = {
     test: 'test value',
+    messages: ['Demo Message'],
 };
 
 const testReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'SET_TEST':
+        case 'LOAD_MESSAGES':
             return Object.assign({}, state, {
-                test: action.test
-            });
+                messages: action.messages,
+        })
+        case 'IMPORT_MESSAGE':
+            const newMessages = state.messages.slice(0);
+            newMessages.unshift(action.message);
+            return Object.assign({}, state, {
+                messages: newMessages,
+            })
         default:
             return state;
     }
